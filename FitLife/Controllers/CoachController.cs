7eb3576c99b634042668ed7b6520bca67ab7ee6a
Sergio Alcázar Monedero <60264314+sergioalcazar745@users.noreply.github.com>
2023-03-01 23:57:06 +0100

    01/03/2023

---
+++ FitLife/Controllers/CoachController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitLife.Controllers
+{
+    public class CoachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
