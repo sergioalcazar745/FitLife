c924f962182fc608ecbf1cf3badf94e213710f06
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-03-16 11:15:58 +0100

    16/03/2023

---
+++ FitLife/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MvcNetCoreSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                RouteValueDictionary rutaLogin = new RouteValueDictionary
+                (
+                    new { controller = "Managed", action = "Login" }
+                );
+                context.Result = new RedirectToRouteResult(rutaLogin);
+            }
+        }
+    }
+}
